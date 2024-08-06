dca19bb61867c2b0085f731318306a337a48c22b
Nikolay Andreychuk <nikolay@testgorilla.com>
2024-08-06 18:28:57 +0100

    Added flagsmith

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI, Query
+from flagsmith import Flagsmith
 
+
+flagsmith = Flagsmith(
+    environment_key="ser.gnV6HasGUwdA8WksEu4KVj", 
+    enable_local_evaluation=True)
 
 app = FastAPI()
 
 @app.get("/{id}")
 def read_root(id: int, language: str | None = Query(None)):
+    traits = {"language": language}
+    identity_flags = flagsmith.get_identity_flags(identifier=str(id), traits=traits)
     return {
-        "feature_enabled": False,
-        "ab_test_flag": False
+        "feature_enabled": identity_flags.is_feature_enabled("my_cool_feature"),
+        "ab_test_flag": identity_flags.get_feature_value("my_cool_ab_test"),
     }
