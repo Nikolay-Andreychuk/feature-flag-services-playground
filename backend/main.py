40bb59465d75c298305c06e91941e4596cac8b97
Nikolay Andreychuk <nikolay@testgorilla.com>
2024-08-09 17:42:30 +0100

    Added redis cache

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,27 @@
+import json
 import logging
 from fastapi import FastAPI, Query
-from growthbook import GrowthBook
+from growthbook import GrowthBook, AbstractFeatureCache, feature_repo
+from redis import Redis
+
+
+class RedisFeatureCache(AbstractFeatureCache):
+  def __init__(self):
+    self.r = Redis(host='redis', port=6379)
+    self.prefix = "gb:"
+
+  def get(self, key: str):
+    data = self.r.get(self.prefix + key)
+    # Data stored as a JSON string, parse into dict before returning
+    return None if data is None else json.loads(data)
+
+  def set(self, key: str, value: dict, ttl: int) -> None:
+    self.r.set(self.prefix + key, json.dumps(value))
+    self.r.expire(self.prefix + key, ttl)
+
+
+# Configure GrowthBook to use your custom cache class
+feature_repo.set_cache(RedisFeatureCache())
 
 
 logging.basicConfig(level=logging.DEBUG)
